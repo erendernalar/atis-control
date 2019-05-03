a9f08635ed4172bd36429c64829f2d8531fa7f7b
xGrond <erendernalar@gmail.com>
2019-05-03 22:40:40 +0300

    first commit

---
+++ imp.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import time
+
+lowerBound=np.array([0,50,200])
+upperBound=np.array([180,128,255])
+
+cam= cv2.VideoCapture("asd.mp4")
+
+kernelOpen=np.ones((5,5))
+kernelClose=np.ones((20,20))
+
+while True:
+    ret, img=cam.read()
+    img=cv2.resize(img,(340,220))
+
+    imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+    mask=cv2.inRange(imgHSV,lowerBound,upperBound)
+
+    maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
+    maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+    maskFinal=maskClose
+
+    _ , conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    
+    cv2.drawContours(img,conts,-1,(255,0,0),3)
+    
+    for i in range(len(conts)):
+        x,y,w,h=cv2.boundingRect(conts[i])
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255), 2)
+	if x > 130 and y > 80:
+		print "READY FOR DROP"
+    cv2.imshow("maskClose",maskClose)
+    cv2.imshow("maskOpen",maskOpen)
+    cv2.imshow("mask",mask)
+    cv2.imshow("cam",img)
+    
+    
+    cv2.imshow("mask",mask)
+    cv2.imshow("cam",img)
+    cv2.waitKey(10)
+    
